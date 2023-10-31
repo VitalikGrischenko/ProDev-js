6dfa67cb980dd0a4fdf1ee816a42db4929b38084
VitalikGrischenko <110487381+VitalikGrischenko@users.noreply.github.com>
2023-10-31 21:11:05 +0200

    dino js

---
+++ src/js/dino.js
@@ -0,0 +1,26 @@
+const dino = document.getElementById("dino-game");
+const cactus = document.getElementById("cactus")
+
+
+function jump() {
+    if(dino.classList != "jump") {
+    dino.classList.add("jump");
+
+    setTimeout(function (){
+        dino.classList.remove("jump")
+    }, 300);
+  }
+}
+
+document.addEventListener("click", function(event) {
+    jump();
+});
+
+var isAlive = setInterval (function () {
+    var dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"))
+    var cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"))
+
+if(cactusLeft <50 && cactusLeft > 0 && dinoTop >= 140) {
+    alert("Game Over")
+}
+}, 10)

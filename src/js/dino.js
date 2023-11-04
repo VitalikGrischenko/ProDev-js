469088e07d4838fd64553a169d71275693c4d02b
VitalikGrischenko <110487381+VitalikGrischenko@users.noreply.github.com>
2023-11-04 15:30:10 +0200

    dino-fix and modal.html

--- src/js/dino.js
+++ src/js/dino.js
@@ -1,6 +1,9 @@
 const dino = document.getElementById("dino-game");
 const cactus = document.getElementById("cactus")
 
+document.addEventListener("click", function(event) {
+    jump();
+});
 
 function jump() {
     if(dino.classList != "jump") {
@@ -12,17 +15,13 @@ function jump() {
   }
 }
 
-document.addEventListener("click", function(event) {
-    jump();
-});
+
 
 var isAlive = setInterval (function () {
     var dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"))
     var cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"))
 
-if(cactusLeft <50 && cactusLeft > 0 && dinoTop >= 140) {
+if(cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
     alert("Game Over")
 }
 }, 10)
-
-

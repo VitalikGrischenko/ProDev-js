faa82be257b1835d6dc80aeafa8dce562616486d
0qsenxx <semen.antipyuk@gmail.com>
2023-11-10 21:54:09 +0200

    lalalla

--- src/main.js
+++ src/main.js
@@ -5,4 +5,6 @@ import './js/our-team';
 import './js/our-team';
 import './js/bigger-number';
 import './js/bigger-number';
+import './js/footer-modal';
+import './js/welcome-modal';
 // import './js/dino';
